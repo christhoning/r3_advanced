f572f5d16e6bd0183eef3d085b946f4ff0844404
Christine Thoning <chtho19@student.sdu.dk>
2024-12-12 09:20:45 +0100

    Fixed "return" error in recipe function

--- R/functions.R
+++ R/functions.R
@@ -74,7 +74,7 @@ metabolites_to_wider <- function(data) {
 #' @param data the lipidomics dataset
 #' @param metabolite_variable the column of interest
 #'
-#' @return data frame
+#' @return recipe object
 create_recipe_spec <- function(data, metabolite_variable) {
   recipes::recipe(data) %>%
     recipes::update_role({{ metabolite_variable }},
