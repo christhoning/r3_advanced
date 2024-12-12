c9af12cdc9f9ff48729188cd987f4be9b90dc1bb
Christine Thoning <chtho19@student.sdu.dk>
2024-12-12 10:06:44 +0100

    Created a ton of models

--- R/functions.R
+++ R/functions.R
@@ -110,3 +110,33 @@ tidy_model_output <- function(workflow_fitted_model) {
     workflows::extract_fit_parsnip() %>%
     broom::tidy(exponentiate = TRUE)
 }
+
+
+#' Convert the long form dataset into a list of wide form data frames based on metabolites
+#'
+#' @param data The lipidomics dataset
+#'
+#' @return A list of data frames
+split_by_metabolite <- function(data) {
+  data %>%
+    columns_values_to_snake_case(metabolite) %>%
+    dplyr::group_split(metabolite) %>% # splits into a list of dataframes
+    purrr::map(metabolites_to_wider)
+}
+
+
+#' Generate the results of a model
+#'
+#' @param data The lipidomics dataset
+#'
+#' @return A data frame
+generate_model_results <- function(data) {
+  create_model_workflow(
+    parsnip::logistic_reg() %>%
+      parsnip::set_engine("glm"),
+    data %>%
+      create_recipe_spec(tidyselect::starts_with("metabolite_"))
+  ) %>%
+    parsnip::fit(data) %>%
+    tidy_model_output()
+}
